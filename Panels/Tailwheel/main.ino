2a79335f98cf82e8c53c2b1587fc0b5383f57828
Claudio <info@gitcd.io>
2024-11-09 17:56:32 +0100

    Works so far

--- Panels/Tailwheel/main.ino
+++ Panels/Tailwheel/main.ino
@@ -1,40 +1,90 @@
 #include <Joystick_ESP32S2.h>
 
-// Create the Joystick
 Joystick_ Joystick;
 
-bool blinkState = false;  //true or false
+bool blinkState = false;
+bool lastBlinkState = false;
+int lastButtonState = 0;
 
+char* lastToggleState = "none";
 
 void setup() {
-  pinMode(D3, OUTPUT);
-  pinMode(D6, INPUT);
-  digitalWrite(D3, LOW);
-  Joystick.begin();
+  pinMode(D7, OUTPUT);
+  digitalWrite(D7, LOW);
 
+
+  pinMode(D3, INPUT_PULLUP);
+  pinMode(D4, INPUT_PULLUP);
+
+  //pinMode(D9, INPUT);
+  //pinMode(D12, INPUT);
+  Joystick.begin();
+  toggleSwitch();
   Serial.begin(9600);
 }
-void loop() {
-
-
-
-
+void buttonPress() {
   bool buttonPressed = digitalRead(D6);
-  Joystick.setButton(0, buttonPressed);
-  if (buttonPressed) {
-    //blinkState = blinkState == false ? true : false;
-    blinkState = !blinkState;
+
+  if (buttonPressed != lastButtonState) {
+    Joystick.setButton(0, buttonPressed);
+    lastButtonState = buttonPressed;
+
+    if (buttonPressed) {
+      blinkState = !blinkState;
+    }
+
   }
 
-  if (blinkState) {
-    digitalWrite(D3, HIGH);
-  } else {
-    digitalWrite(D3, LOW);
+  //Serial.println("buttonPressed");
+  //Serial.println(buttonPressed);
+
+  if (blinkState != lastBlinkState){
+    if (blinkState) {
+      digitalWrite(D7, HIGH);
+    } else {
+      digitalWrite(D7, LOW);
+    }
+    lastBlinkState = blinkState;
+  }
+}
+
+void toggleSwitch() {
+  bool off = digitalRead(D3);
+  bool fixed = digitalRead(D4);
+
+  char* toggleState;
+  if (fixed && !off) {
+    toggleState = "fixed";
+    //Joystick.setButton(1, true);
+  } else if (fixed && off) {
+    toggleState = "norm";
+    //Joystick.setButton(2, true);
+  } else if (!fixed && off) {
+    toggleState = "off";
+    //Joystick.setButton(3, true);
   }
 
-  if (buttonPressed) {
-    delay(500);
+  if (toggleState != lastToggleState){
+    if (toggleState == "fixed") {
+      Joystick.setButton(1, true);
+      Joystick.setButton(2, false);
+      Joystick.setButton(3, false);
+    } else if (toggleState == "norm") {
+      Joystick.setButton(1, false);
+      Joystick.setButton(2, true);
+      Joystick.setButton(3, false);
+    } else if (toggleState == "off") {
+      Joystick.setButton(1, false);
+      Joystick.setButton(2, false);
+      Joystick.setButton(3, true);
+    }
+    lastToggleState = toggleState;
   }
 
+}
 
+void loop() {
+  buttonPress();
+  toggleSwitch();
+  delay(10);
 }
